269d87018231be8efbb8bbbeaa047167b0c4d067
Jaden Chant <chant1@stolaf.edu>
2020-12-20 22:22:06 -0600

    added more to practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+function formatUser(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Jaden',
+  lastName: 'Chant'
+}
+
+const welcome = (
+  <h2>
+    Nice to meet you!
+  </h2>
+);
+
+class FirstTime extends React.Component {
+  render() {
+    let statement;
+    if(this.props.isFirstTime) {
+      statement = welcome;
+    } else {
+      statement = <h2>Welcome back</h2>
+    }
+    return statement;
+  }
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearImmediate(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <h4>It is {this.state.date.toLocaleTimeString()}</h4>
+    )
+  }
+}
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: false};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }))
+    if(this.state == {isToggleOn: true}) {
+
+    }
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+      <h1>Hello, {formatUser(user)}!</h1>
+      {/* To use Boolean Values true is present and false is omitted */}
+      <FirstTime isFirstTime='true'></FirstTime>
+      <Clock />
+      <Toggle style={{color: 'red'}} />
     </div>
   );
 }
